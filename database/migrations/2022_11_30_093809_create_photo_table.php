e49be13415c4a5ddb1c3039464a118abecb4a950
lati111 <jvzelst111@gmail.com>
2022-11-30 10:43:49 +0100

    Added photo model/db

---
+++ database/migrations/2022_11_30_093809_create_photo_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 40);
+            $table->string("author", 40);
+            $table->string("generator", 40);
+            $table->text("prompt");
+            $table->integer("height");
+            $table->integer("width");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo');
+    }
+};
